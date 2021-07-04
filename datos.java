d50741fdeb269f942751bc4b19d189e1425d4947
DiegoGuisasola <diego.guisasola@utp.edu.co>
2021-07-04 02:13:02 -0500

    dasds

---
+++ datos.java
@@ -0,0 +1,15 @@
+public class datos {
+    public static void main(String[] args) {
+        char letra1 = 'h';
+        char letra2 = 'o';
+        char letra3 = 'l';
+        char letra4 = 'a';
+
+        System.out.print(letra1);
+        System.out.print(letra2);
+        System.out.print(letra3);
+        System.out.println(letra4);
+
+        System.out.print(letra1 + "" + letra2 + "" + letra3 + "" + letra4); // '' No funciona.
+    }
+}
---
+++ operaciones.java
@@ -0,0 +1,25 @@
+public class operaciones {
+    public static void main(String[] args) {
+
+        int var1 = 5;
+        int var2 = 4;
+
+        int suma = var1 + var2;
+        int resta = var1 - var2;
+        int multiplicacion = var1 * var2;
+        double division = var1 / var2;
+
+        // Suma
+        System.out.println("La Suma es: " + suma);
+
+        // Resta
+        System.out.println("La Resta es: " + resta);
+
+        // Multiplicacion
+        System.out.println("La Multiplicacion es: " + multiplicacion);
+
+        // Division
+        System.out.println("La Division es: " + division);
+
+    }
+}
