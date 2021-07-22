ebe4479823178add8ca58c506b9cdfc6fd017adc
Diego Mac <diego.guisasola@utp.edu.co>
2021-07-21 22:53:41 -0500

    Ejercicio con operador ternario

--- clase1/proyectoClase1/src/Ejercicio1.java
+++ clase1/proyectoClase1/src/Ejercicio1.java
@@ -6,6 +6,18 @@ public class Ejercicio1 {
         String cadenaSaludo = "Hola " + nombre;
         return cadenaSaludo;
     }
+
+    /* 
+
+    // public: Auemnta el alcance para ser utilizado en las otras clases del proyecto.
+    // Sin static: Toca instanciar. Sólo se podría usar en este archivo. Copiar todo en los demas archivos que necesiten esta funcion
+    nombreClase instancia = new nombreClase();
+    instancia.nombreMetodoALlamar();
+    
+    // Con static: No toca instanciar. Libera la función para ser utilizada en otro  lugar. Aumenta el alcance.
+    nombreClase.nombreMetodoALlamar();
+    */
+    
     public static void main(String[] args) {
         String nombre = "Tripulante P61";
 
@@ -13,4 +25,5 @@ public class Ejercicio1 {
 
         System.out.println(saludoCompleto);
     }
-}
+} 
+
--- clase1/proyectoClase1/src/Ejercicio2.java
+++ clase1/proyectoClase1/src/Ejercicio2.java
@@ -1,5 +1,22 @@
 public class Ejercicio2 {
     public static void main(String[] args) {
-        
+        int a = 1;
+        int b = 3;
+        int result = 0;
+        // Verificacion:
+        if (a < b) {
+            result = a;
+        } else {
+            result = b;
+        }
+
+        System.out.println("1: " + result);
+
+        // If else compacto. Operador ternario: http://lineadecodigo.com/java/el-operador-ternario-en-java/
+        int result2 = (a < b) ? b : a;
+
+        System.out.println("2: " + result2);
+
+
     }
 }
